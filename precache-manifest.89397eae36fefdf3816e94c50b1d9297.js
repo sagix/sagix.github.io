30bfbb43d662bb96c18da9973b44046553e2da96
Nicolas Mouchel <nmo@octo.com>
2019-06-02 17:08:33 +0200

    upgrade to react 3

---
+++ precache-manifest.89397eae36fefdf3816e94c50b1d9297.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c5aeaa0a484ae59345d946fb73a1e7f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fbf5560c31bf53a58dfe",
+    "url": "/static/css/main.6721b0cc.chunk.css"
+  },
+  {
+    "revision": "66db9d8395a974ebb1cc",
+    "url": "/static/js/2.626ab0c6.chunk.js"
+  },
+  {
+    "revision": "fbf5560c31bf53a58dfe",
+    "url": "/static/js/main.e03e4dc6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "921a0631b36b980b57d6f4366d5dba8f",
+    "url": "/static/media/auto-fix.921a0631.svg"
+  },
+  {
+    "revision": "e1de1093070429c451d5558cd461e299",
+    "url": "/static/media/empty.e1de1093.svg"
+  },
+  {
+    "revision": "743ec13d6154e9da747750ae549cd960",
+    "url": "/static/media/logo.743ec13d.svg"
+  },
+  {
+    "revision": "eeafdbc31a015191b884d2452cc44ecf",
+    "url": "/static/media/weather-night.eeafdbc3.svg"
+  },
+  {
+    "revision": "d2d3cc2fbca97ce6d5301a43c19af94a",
+    "url": "/static/media/weather-none.d2d3cc2f.svg"
+  },
+  {
+    "revision": "fb4f8a6b014da92430732dae563e4715",
+    "url": "/static/media/weather-sunny.fb4f8a6b.svg"
+  },
+  {
+    "revision": "2a4daadf8044c46f09ed5a439c9e0fde",
+    "url": "/static/media/weather-sunset-down.2a4daadf.svg"
+  },
+  {
+    "revision": "65e6d39da97d64db826a4c4447b4e2c7",
+    "url": "/static/media/weather-sunset-up.65e6d39d.svg"
+  }
+]);
